92fd3610adec9b23f62065a384abbf6f58b59da9
Anne Heimes <aheimes@posteo.de>
2022-09-23 11:24:47 +0200

    clean up tests

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+import numpy as np
+import pyfar as pf
+from imkar.testing import stub_utils
+
+
+@pytest.fixture
+def coords_half_sphere_1_deg():
+    delta = 1
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi/2, num=int(90/delta)+1))
+
+
+@pytest.fixture
+def coords_half_sphere_5_deg():
+    delta = 5
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi/2, num=int(90/delta)+1))
+
+
+@pytest.fixture
+def coords_half_sphere_10_deg():
+    delta = 10
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi/2, num=int(90/delta)+1))
+
+
+@pytest.fixture
+def coords_half_sphere_30_deg():
+    delta = 30
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi/2, num=int(90/delta)+1))
+
+
+@pytest.fixture
+def coords_half_sphere_45_deg():
+    delta = 45
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi/2, num=int(90/delta)+1))
+
+
+@pytest.fixture
+def coords_sphere_10_deg():
+    delta = 10
+    return stub_utils.create_coordinates_sph(
+        np.linspace(0, 2*np.pi, num=int(360/delta)+1),
+        np.linspace(0, np.pi, num=int(180/delta)+1))
--- tests/test_scattering_coefficient.py
+++ tests/test_scattering_coefficient.py
@@ -3,16 +3,17 @@ import numpy as np
 
 from imkar import scattering
 from pyfar import FrequencyData, Coordinates
+from imkar.testing import stub_utils
 
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_1(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    p_sample = _create_frequencydata(mics.cshape, 1, frequencies)
-    p_reference = _create_frequencydata(mics.cshape, 0, frequencies)
+def test_freefield_1(frequencies, coords_half_sphere_10_deg):
+    mics = coords_half_sphere_10_deg
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 1, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
     p_sample.freq[5, 0, :] = 0
     p_reference.freq[5, 0, :] = np.sum(p_sample.freq.flatten())/2
     s = scattering.coefficient.freefield(p_sample, p_reference, mics)
@@ -21,12 +22,12 @@ def test_freefield_1(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_wrong_input(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    p_sample = _create_frequencydata(mics.cshape, 1, frequencies)
-    p_reference = _create_frequencydata(mics.cshape, 0, frequencies)
+def test_freefield_wrong_input(frequencies, coords_half_sphere_10_deg):
+    mics = coords_half_sphere_10_deg
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 1, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
     p_sample.freq[5, 0, :] = 0
     p_reference.freq[5, 0, :] = np.sum(p_sample.freq.flatten())/2
     with pytest.raises(ValueError, match='p_sample'):
@@ -41,12 +42,12 @@ def test_freefield_wrong_input(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_05(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    p_sample = _create_frequencydata(mics.cshape, 0, frequencies)
-    p_reference = _create_frequencydata(mics.cshape, 0, frequencies)
+def test_freefield_05(frequencies, coords_half_sphere_10_deg):
+    mics = coords_half_sphere_10_deg
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
     p_sample.freq[5, 7, :] = 1
     p_sample.freq[5, 5, :] = 1
     p_reference.freq[5, 5, :] = 1
@@ -56,12 +57,12 @@ def test_freefield_05(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_0(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    p_sample = _create_frequencydata(mics.cshape, 0, frequencies)
-    p_reference = _create_frequencydata(mics.cshape, 0, frequencies)
+def test_freefield_0(frequencies, coords_half_sphere_10_deg):
+    mics = coords_half_sphere_10_deg
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        mics.cshape, 0, frequencies)
     p_reference.freq[5, 0, :] = 1
     p_sample.freq[5, 0, :] = 1
     s = scattering.coefficient.freefield(p_sample, p_reference, mics)
@@ -71,15 +72,15 @@ def test_freefield_0(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_0_with_inci(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    incident_directions = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=8), np.linspace(0, np.pi/2, num=4))
+def test_freefield_0_with_inci(
+        frequencies, coords_half_sphere_10_deg, coords_half_sphere_30_deg):
+    mics = coords_half_sphere_10_deg
+    incident_directions = coords_half_sphere_30_deg
     data_shape = np.array((incident_directions.cshape, mics.cshape)).flatten()
-    p_sample = _create_frequencydata(data_shape, 0, frequencies)
-    p_reference = _create_frequencydata(data_shape, 0, frequencies)
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
     p_reference.freq[:, :, 1, 2, :] = 1
     p_sample.freq[:, :, 1, 2, :] = 1
     s, s_rand = scattering.coefficient.freefield(
@@ -93,15 +94,15 @@ def test_freefield_0_with_inci(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_1_with_inci(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=19))
-    incident_directions = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=8), np.linspace(0, np.pi/2, num=4))
+def test_freefield_1_with_inci(
+        frequencies, coords_half_sphere_10_deg, coords_half_sphere_30_deg):
+    mics = coords_half_sphere_10_deg
+    incident_directions = coords_half_sphere_30_deg
     data_shape = np.array((incident_directions.cshape, mics.cshape)).flatten()
-    p_sample = _create_frequencydata(data_shape, 0, frequencies)
-    p_reference = _create_frequencydata(data_shape, 0, frequencies)
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
     p_reference.freq[:, :, 1, 2, :] = 1
     p_sample.freq[:, :, 2, 3, :] = 1
     s, s_rand = scattering.coefficient.freefield(
@@ -115,15 +116,15 @@ def test_freefield_1_with_inci(frequencies):
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_freefield_05_with_inci(frequencies):
-    mics = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi, num=10))
-    incident_directions = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=8), np.linspace(0, np.pi/2, num=4))
+def test_freefield_05_with_inci(
+        frequencies, coords_half_sphere_10_deg, coords_half_sphere_30_deg):
+    mics = coords_half_sphere_10_deg
+    incident_directions = coords_half_sphere_30_deg
     data_shape = np.array((incident_directions.cshape, mics.cshape)).flatten()
-    p_sample = _create_frequencydata(data_shape, 0, frequencies)
-    p_reference = _create_frequencydata(data_shape, 0, frequencies)
+    p_sample = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
+    p_reference = stub_utils.create_const_frequencydata_from_shape(
+        data_shape, 0, frequencies)
     p_sample.freq[:, :, 5, 7, :] = 1
     p_sample.freq[:, :, 5, 5, :] = 1
     p_reference.freq[:, :, 5, 5, :] = 1
@@ -140,44 +141,27 @@ def test_freefield_05_with_inci(frequencies):
     (0), (0.2), (0.5), (0.8), (1)])
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_random_incidence_constant_s(s_value, frequencies):
-    incident_directions = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi/2, num=10))
-    s = _create_frequencydata(incident_directions.cshape, s_value, frequencies)
+def test_random_incidence_constant_s(
+        s_value, frequencies, coords_half_sphere_10_deg):
+    incident_directions = coords_half_sphere_10_deg
+    s = stub_utils.create_const_frequencydata_from_shape(
+        incident_directions.cshape, s_value, frequencies)
     s_rand = scattering.coefficient.random_incidence(s, incident_directions)
     np.testing.assert_allclose(s_rand.freq, s_value)
 
 
 @pytest.mark.parametrize("frequencies",  [
     ([100, 200]), ([100])])
-def test_random_incidence_non_constant_s(frequencies):
-    incident_directions = _create_coordinates(
-        np.linspace(0, 2*np.pi, num=37),
-        np.linspace(0, np.pi/2, num=10))
+def test_random_incidence_non_constant_s(
+        frequencies, coords_half_sphere_10_deg):
+    incident_directions = coords_half_sphere_10_deg
     s_value = np.arange(37*10).reshape(incident_directions.cshape) / 370
     sph = incident_directions.get_sph()
     actual_weight = np.sin(2*sph[..., 1])   # sin(2*theta)
     actual_weight /= np.sum(actual_weight)
-    s = _create_frequencydata(incident_directions.cshape, s_value, frequencies)
+    s = stub_utils.create_frequencydata_from_shape(
+        [], s_value, frequencies)
     s_rand = scattering.coefficient.random_incidence(s, incident_directions)
     desired = np.sum(s_value*actual_weight)
     np.testing.assert_allclose(s_rand.freq, desired)
 
-
-def _create_coordinates(phi_rad, theta_rad):
-    phi, theta = np.meshgrid(phi_rad, theta_rad)
-    coords = Coordinates(
-        phi, theta, np.ones(phi.shape), 'sph')
-    return coords
-
-
-def _create_frequencydata(shape, data_raw, frequencies):
-    frequencies = np.atleast_1d(frequencies)
-    shape_new = np.append(shape, frequencies.shape)
-    if hasattr(data_raw, "__len__"):
-        data_raw = np.repeat(
-            data_raw[..., np.newaxis], len(frequencies), axis=-1)
-    data = np.zeros(shape_new) + data_raw
-    p_reference = FrequencyData(data, frequencies)
-    return p_reference
