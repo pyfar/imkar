42193aa15f767f0d1587f1c09a0d69777f28972d
ahms5 <ahe@akustik.rwth-aachen.de>
2022-11-30 14:17:18 +0100

    add test for non uniform surface

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -59,7 +59,7 @@ def test_surface_sphere_data_preserve_shape_with_different_limits(
         (0,         2*np.pi,    np.pi/2,    np.pi,      4*np.pi/2),
         (np.pi-.1,  2*np.pi-.1, 0,          np.pi,      4*np.pi/2),
     ])
-def test_surface_sphere_nonuniform_data_different_limits(
+def test_surface_sphere_uniform_data_different_limits(
         phi_0, phi_1, theta_0, theta_1, desired):
     delta = np.pi/18
     coords = stub_utils.spherical_coordinates(
@@ -91,3 +91,32 @@ def test_surface_sphere_error_invalid_coordinates_shape(coords_sphere_10_deg):
     coords.set_sph(sph[..., 0], sph[..., 1], sph[..., 2])
     with pytest.raises(ValueError, match='Coordinates.cshape'):
         integrate.surface_sphere(data, coords)
+
+
+@pytest.mark.parametrize(
+    "   phi_0,      phi_1,      theta_0,    theta_1",  [
+        (0,         2*np.pi,    0,          np.pi),
+        (0,         np.pi,      0,          np.pi),
+        (0,         np.pi,      0,          np.pi/2),
+        (np.pi,     2*np.pi,    0,          np.pi/2),
+        (np.pi-.1,  2*np.pi-.1, 0,          np.pi/2),
+        (0,         2*np.pi,    np.pi/2,    np.pi),
+        (np.pi-.1,  2*np.pi-.1, 0,          np.pi),
+        (np.pi,     2*np.pi,    0,          np.pi),
+    ])
+def test_surface_sphere_nonuniform_data_different_limits(
+        phi_0, phi_1, theta_0, theta_1):
+    delta = np.pi/18
+    coords = stub_utils.spherical_coordinates(
+        np.arange(phi_0, phi_1+delta, delta),
+        np.arange(theta_0, theta_1+delta, delta))
+    data = stub_utils.frequencydata_from_shape(coords.cshape, 1, 100)
+    theta = coords.get_sph()[..., 1]
+    # data.freq = data.freq[0, ...]
+    data.freq[..., 0] = np.cos(theta)
+    result = integrate.surface_sphere(data, coords)
+    actual = np.real(result.freq[0, 0])
+    theta_upper = -np.cos(2*theta_1)/4
+    theta_lower = -np.cos(2*theta_0)/4
+    desired = (theta_upper - theta_lower) * (phi_1 - phi_0)
+    np.testing.assert_allclose(actual, desired, rtol=5e-3, atol=0.04)
