93154657c7d4ba8a7dfc5e6983af55ebc1da6be0
Max Nilo <Maxnilo1234@gmail.com>
2023-01-20 15:12:31 +0100

    First version of analytical function.

--- imkar/analytical/analytical.py
+++ imkar/analytical/analytical.py
@@ -47,7 +47,7 @@ def rectangular(frequency_vector, phi_vector, width, length, height_to_length):
     """
     # Initialization
     phi_vector = phi_vector*np.pi/180 #from degree to radiant
-    c = 343.2 # 20°C
+    c = 343.901 # ITA constant
     h = height_to_length*length #height of the profile
     lambda_air = c/frequency_vector[:] #vector of corresponding wavelength in air
     k = 2*np.pi/lambda_air #vector of wavenumbers
@@ -56,9 +56,9 @@ def rectangular(frequency_vector, phi_vector, width, length, height_to_length):
     n_max = 50 # truncation parameter for numbers of outgoing waves
     n_max2 = 2*n_max+1 #counter from -N to N including zero
 
-    x_u = np.zeros((1, n_max))
+    x_u = np.zeros((1, n_max),dtype=complex)
     n = np.arange(-n_max,n_max+1)[:, np.newaxis]
-    index_0 = np.nonzero(n==0) # index for R_0
+    #index_0 = 51 # index for R_0
     previous_frequency = 0
 
     n_frequency = frequency_vector.size # number of computed frequencies
@@ -70,7 +70,8 @@ def rectangular(frequency_vector, phi_vector, width, length, height_to_length):
         for iPhi in range(1, (n_phi+1)):
             phi_0 = phi_vector[iPhi-1]
             alpha_n = np.cos(phi_0) + n*lambda_air[iFrequencies-1]/length
-            beta_n = 1j*np.emath.sqrt(alpha_n**2-1)#np.sin(np.arccos(alpha_n)) 
+            beta_n = np.conj(-(1j*np.emath.sqrt(alpha_n**2-1)))
+            #np.sin(np.emath.arccos(alpha_n))            
 
             #helping variables
             k_frequency = k[iFrequencies-1]
@@ -89,56 +90,61 @@ def rectangular(frequency_vector, phi_vector, width, length, height_to_length):
                     err = 1
                     while err>eps:
                         u_max[0,iR-1] = u_max[0,iR-1] + n_max2-1
+                        #print(u_max)
+                        #print(u_max.item((iR-1)))
 
                         if iR==1:
                             U_u_n = np.zeros((u_max.item((iR-1))+1,n_max2),dtype=complex)
                         elif u_max[0,iR-1]>u_max_element:
-                            U_u_n = np.zeros((u_max.item((iR-1))+1, n_max2)) 
+                            U_u_n = np.zeros((u_max.item((iR-1))+1, n_max2),dtype=complex) 
                         
-                        if iR==1 or u_max[0,iR-1]>u_max_element or iFrequencies>previous_frequency:
-                            iU = np.arange(0., round(u_max.item((iR-1))),dtype=int)[:, np.newaxis]
+                        if ((iR==1) or (u_max[0,iR-1]>u_max_element) or (iFrequencies>previous_frequency)):
+                            iU = np.arange(0., round(u_max.item((iR-1)))+1,dtype=int)[:, np.newaxis]                           
                             uVals = np.tile(iU, (1, n_max2))
                             k_x_u = iU*np.pi/(2*width)
                             x_u = np.emath.sqrt(k_frequency**2 - k_x_u**2)
 
                             CaseA = np.less(abs((k_alpha_n-k_x_u)), 1E-3)
                             CaseA = CaseA.astype(int)
-                            CaseB = np.greater(abs((k_alpha_n+k_x_u)), 1E-3)
+                            CaseB = np.less(abs((k_alpha_n+k_x_u)), 1E-3)
                             CaseB = CaseB.astype(int)
                             CaseC = np.ones(np.shape(CaseA))-(CaseA+CaseB)
-                            CaseC = CaseC.astype(int)
-                            U_u_n[CaseA] = 0.5*np.exp(1j*uVals[CaseA]*np.pi/2)
-                            U_u_n[CaseB] = 0.5*np.exp(-1j*uVals[CaseB]*np.pi/2)
+                            CaseC = CaseC.astype(int) 
+                            if(np.sum(CaseA)>0):                           
+                                U_u_n[np.nonzero(CaseA)] = 0.5*np.exp(1j*uVals[CaseA]*np.pi/2)
+                            if(np.sum(CaseB)>0):
+                                U_u_n[np.nonzero(CaseB)] = 0.5*np.exp(-1j*uVals[CaseB]*np.pi/2)
                             tmp = (jk_alpha_n_over_2width/\
                                 (k_x_u**2-k_alpha_n**2))*((np.exp(1j*k_alpha_n*width))-\
-                                    (-1**uVals/(np.exp(1j*k_alpha_n*width))))
-                            U_u_n[CaseC] = tmp[CaseC]
+                                    ((-1)**uVals*(np.exp(-1j*k_alpha_n*width))))  
+                            #print(np.shape(tmp))                      
+                            U_u_n[np.nonzero(CaseC)] = tmp[np.nonzero(CaseC)]
 
                         tempU = np.sum([(np.sign(iU)+1)*x_u*np.tanh(1j*x_u*h)\
-                            *np.conjugate(U_u_n[iU,iN-1])*U_u_n[iU,iR-1]])
+                            *np.conjugate(U_u_n[iU,iN-1])*U_u_n[iU,iR-1]],axis=1)
 
                         err = np.abs((prevTempU-tempU)/prevTempU)
                         prevTempU = tempU
-                        previous_frequency = iFrequencies
+                        previous_frequency = iFrequencies                        
 
                         if u_max_element<u_max.item((iR-1)):
                             u_max_element = u_max.item((iR-1))
                         
                     y_nr[iN-1,iR-1] = tempU*2*width/(k_frequency*length)
 
-            b = np.sin(phi_0)*(2-(abs(np.sign(n))+1)) + y_nr[:,n_max]
-            print(b)
-            A = np.diag(beta_n) -y_nr
+            b = np.sin(phi_0)*(2-(abs(np.sign(n))+1)) + ((y_nr[:,n_max]).reshape(n_max2,1))
+            A = np.diagflat(beta_n) -y_nr
 
             R_n = np.linalg.lstsq(A,b, 1E-6)
-            print (R_n)
+            #print (R_n[0][n_max])
             #validIds = np.abs(alpha_n)<=1
             #checksum = np.sum((np.abs(R_n[validIds])**2)\
             #    *beta_n[validIds]/np.sin(phi_0))
             #absError = np.abs(checksum-1)
             s[iFrequencies-1, iPhi-1] = 1-np.abs(R_n[0][n_max])**2
+            print(s)
 
-    s.comment = 'Analytical solution of the scattering coefficient\
-         of rectangular profiles'
+    #s.comment = 'Analytical solution of the scattering coefficient\
+    #     of rectangular profiles'
     return s
 
