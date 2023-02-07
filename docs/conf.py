c85d9a27ba03de649d6d39bfa948b7bf1174796b
Anne Heimes <aheimes@posteo.de>
2023-02-07 09:35:46 +0100

    apply doc from pyfar

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-# imkar documentation build configuration file, created by
+# pyfar documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun  9 13:47:02 2017.
 #
 # This file is execfile()d with the current directory set to its
@@ -21,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import imkar
+import imkar  # noqa
 
 # -- General configuration ---------------------------------------------
 
@@ -31,10 +32,25 @@ import imkar
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'matplotlib.sphinxext.plot_directive',
+    'sphinx.ext.mathjax',
+    'autodocsumm']
+
+# show tocs for classes and functions of modules using the autodocsumm
+# package
+autodoc_default_options = {'autosummary': True}
+
+# show the code of plots that follows the command .. plot:: based on the
+# package matplotlib.sphinxext.plot_directive
+plot_include_source = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -64,26 +80,32 @@ release = imkar.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# The name of the Pygments (syntax highlighting) style to use (Not defining
+# uses the default style of the html_theme).
+# pygments_style = 'sphinx'
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# default language for highlighting in source code
+highlight_language = "python3"
 
 # -- Options for HTML output -------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'insipid'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -94,13 +116,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'imkardoc'
+htmlhelp_basename = 'pyfardoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -153,10 +175,7 @@ texinfo_documents = [
     (master_doc, 'imkar',
      'imkar Documentation',
      author,
-     'imkar',
+     'pyfar',
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,50 @@
-Welcome to imkar's documentation!
-======================================
+.. |imkar_logo| image:: resources/imkar.png
+   :width: 150
+   :alt: Alternative text
+
+|imkar_logo|
+
+
+Getting Started
+===============
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
    readme
-   installation
-   usage
+
+Documentation
+=============
+
+.. toctree::
+   :maxdepth: 2
+
+   concepts
+
+.. toctree::
+   :maxdepth: 2
+
    modules
+
+Contributing
+===============
+
+.. toctree::
+   :maxdepth: 2
+
    contributing
-   authors
+
+Other
+=====
+.. toctree::
+   :maxdepth: 1
+
    history
 
-Indices and tables
-==================
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+..
+   (removed because it is contained in the new spinx style)
+   Indices and tables
+   ==================
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
