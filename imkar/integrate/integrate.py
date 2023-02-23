93901db8c801c49a84e157b372053842b822c6fc
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-02-23 17:17:48 +0100

    weights can be manipulated now

--- imkar/integrate/integrate.py
+++ imkar/integrate/integrate.py
@@ -4,7 +4,7 @@ import numpy as np
 import pyfar as pf
 
 
-def surface_sphere(data, coords):
+def surface_sphere(data, coords, weights=None):
     r"""Integrate over a set of points sampled on a spherical surface.
 
     .. math::
@@ -17,8 +17,12 @@ def surface_sphere(data, coords):
         Input data to integrate. Its `cshape` needs to be (..., #theta, #phi)
         or (..., #phi, #theta).
     coords : Coordinates
-        Coordinate points at which the data is sampled. Its cshape needs to be
-        (#theta, #phi) or (#phi, #theta), matching the cshape of `data`.
+        Coordinate points at which the data is sampled. Its `cshape` needs to
+        be (#theta, #phi) or (#phi, #theta), matching the `cshape` of `data`.
+    weights : np.array
+        weights for the integration over the coordinates. Its `cshape`
+        needs to be same as for `coords`.
+        By default the its sin(theta).
 
     Returns
     -------
@@ -51,27 +55,29 @@ def surface_sphere(data, coords):
     coords_spherical = coords.get_sph(convention='top_colat')
     phi = coords_spherical[1, :, 0]
     theta = coords_spherical[:, 1, 1]
-    axis_index = -2
+    axis_index = -1
     if np.sum(np.diff(phi[1:-2])) < 1e-3:
         phi = coords_spherical[:, 1, 0]
         theta = coords_spherical[1, :, 1]
-        axis_index = -3
+        axis_index = -2
     radius = coords_spherical[:, :, 2]
     r_mean = np.mean(radius)
     if not np.allclose(radius, r_mean):
         warnings.warn(r'all radii should be almost same')
 
-    # pf.Coordinates turns phi = 2*pi to 0, due to cylindircal
+    # pf.Coordinates turns phi = 2*pi to 0, due to circular
     # for the integration the upper limit cannot be zero but 2pi
     last_phi_is_zero = np.isclose(phi[-1], 0, atol=1e-16)
     increasing_phi_to_2pi = np.isclose(
         phi[-2]+np.diff(phi)[0], 2*np.pi, atol=1e-16)
     if last_phi_is_zero and increasing_phi_to_2pi:
-        phi[-1] = 2*np.pi
+        phi[-1] = 2 * np.pi
 
-    # integrate over angles with wight for sperical integration
-    weights = np.transpose(np.atleast_2d(np.sin(theta)))
-    result_raw = trapezoid(data.freq, x=phi, axis=axis_index)
-    result_raw1 = trapezoid(result_raw*weights, x=theta, axis=-2)
+    # integrate over angles with wight for spherical integration
+    data_int = np.moveaxis(data.freq, -1, 0)
+    if weights is None:
+        weights = np.sin(coords_spherical[..., 1])
+    result_raw = trapezoid(data_int*weights, x=phi, axis=axis_index)
+    result_raw1 = trapezoid(result_raw, x=theta, axis=-1)
 
-    return pf.FrequencyData(result_raw1, data.frequencies)
+    return pf.FrequencyData(np.moveaxis(result_raw1, 0, -1), data.frequencies)
