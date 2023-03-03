eeb6c2e15602f8e61b895f6fd05f9e51e36de505
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-03-03 15:17:57 +0100

    revert conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 #
 # imkar documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun  9 13:47:02 2017.
@@ -22,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import imkar  # noqa
+import imkar
 
 # -- General configuration ---------------------------------------------
 
@@ -32,24 +31,10 @@ import imkar  # noqa
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'autodocsumm']
-
-# show tocs for classes and functions of modules using the autodocsumm
-# package
-autodoc_default_options = {'autosummary': True}
-
-# show the code of plots that follows the command .. plot:: based on the
-# package matplotlib.sphinxext.plot_directive
-plot_include_source = True
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -79,32 +64,26 @@ release = imkar.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# The name of the Pygments (syntax highlighting) style to use (Not defining
-# uses the default style of the html_theme).
-# pygments_style = 'sphinx'
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-add_function_parentheses = False
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-# default language for highlighting in source code
-highlight_language = "python3"
 
 # -- Options for HTML output -------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'insipid'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -115,7 +94,7 @@ html_theme = 'insipid'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
@@ -178,3 +157,6 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
+
+
+
