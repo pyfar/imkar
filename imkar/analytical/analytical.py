bc64773e7a94871e9f05be47be206e0b08c63b18
Max Nilo <Maxnilo1234@gmail.com>
2023-04-05 15:22:39 +0200

    inputs changed

--- imkar/analytical/analytical.py
+++ imkar/analytical/analytical.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pyfar as pf
 
-def sinusoid(frequency_vector, phi_vector, theta):
+def sinusoid(frequencies, phis, theta, length=0.177, height=0.0255, c=343):
     """
     This function computes the scattering coefficient of a sine-shaped surface
     (with infinite extend) for a given set of frequencies and angles.
@@ -19,6 +19,8 @@ def sinusoid(frequency_vector, phi_vector, theta):
     theta : double
         Out-of-plane incident angle of incoming waves. Values between -90°-90°
         allowed.
+    c : double
+        Speed-of-sound used for the computation.
 
     Returns
     -------
@@ -35,20 +37,17 @@ def sinusoid(frequency_vector, phi_vector, theta):
     # check inputs
 
     # Initialization and constants
-    c = 343.901  # ITA constant
     phi_vector = phi_vector*np.pi/180  # from degree to radiant
-    length = 0.354/2  #structural length
-    height = 0.051/2  #structural height
     k_structural = 2*np.pi/length  # structural wavenumber
     # vector of corresponding wavelength in air
-    lambda_air = c/frequency_vector[:]
+    lambda_air = c/frequencies
     k = 2*np.pi/lambda_air
     k_theta = k*np.sin(theta)  # account out-of-plane incidence
-    eta0 = 0
+    eta0 = 0  # normalized admittance
 
     # order of maximum reflection
-    n_max = np.ceil(2*length*frequency_vector.max()/c)
-    s_coeff = np.zeros((frequency_vector.size(),phi_vector.size()))
+    n_max = np.ceil(2*length*frequencies.max()/c)
+    s_coeff = np.zeros((frequencies.size(),phis.size()))
 
 
 
