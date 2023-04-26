8d4acad71a6c7dfd86088b459eb4c5ed00a202e2
Max Nilo <Maxnilo1234@gmail.com>
2023-04-26 16:42:18 +0200

    Improved readability and added suggestions

--- imkar/scattering/analytical.py
+++ imkar/scattering/analytical.py
@@ -4,7 +4,8 @@ import pyfar as pf
 import numbers
 
 
-def rectangular(frequencies, phis, width, length, height, c=343):
+def rectangular(frequencies, incident_angles, rectangle_width, gap_width,
+                height, speed_of_sound=343):
     """
     This function computes the scattering coefficient of periodic rectangular
     profiles, assuming that the extent is infinite.
@@ -14,24 +15,24 @@ def rectangular(frequencies, phis, width, length, height, c=343):
 
     Parameters
     ----------
-    frequencies : array, double
+    frequencies : array, float
         Vector of frequencies the scattering coefficient is computed at.
-    phis : array, double
-        Vector of incidences phi the scattering coefficient is computed at.
-        Values between 0°-90° allowed.
-    width : double
+    incident_angles : array, float
+        Vector or value of incident angles in degree for the scattering
+        coefficient calculation. Angles between 0°-90° are allowed.
+    rectangle_width : float
         Width of the rectangulars in m.
-    length : double
-        Length of the regtangulars in m.
-    height : double
-        Height of the profile structure.
-    c : double
+    gap_width : float
+        Width of the gap in m.
+    height : float
+        Height of the profile structure in m.
+    speed_of_sound : float
         Speed of sound with which the scattering coefficient is computed.
     Returns
     -------
-    s : FrequencyData
+    scattering_coefficient : FrequencyData
         The analytical solution for the scattering coefficient of a
-        rectangular profile at the given incidences and frequencies.
+        rectangular profile at the given incident angles and frequencies.
 
     References
     ----------
@@ -44,61 +45,62 @@ def rectangular(frequencies, phis, width, length, height, c=343):
     Comment
     -------
     In this solution the parameter r in equation (9) is also limited to
-    -N <= r <= N. The width of the well is described by the width parameter and
-    replaces the parameter 2A.
+    -N <= r <= N. The width of the well is described by the gap_width
+    parameter and replaces the parameter 2A.
 
     """
 
     # check inputs
     if not isinstance(frequencies, np.ndarray) or\
             not isinstance(frequencies.item(0), numbers.Real):
-        raise TypeError("frequency_vector has to be an array of real numbers")
+        raise TypeError("frequencies has to be an array of real numbers")
     if frequencies.ndim > 1:
-        raise ValueError("frequency_vector has to be 1-dimensional")
-    if not isinstance(phis, np.ndarray) or \
-            not isinstance(phis.item(0), numbers.Real):
-        raise TypeError("phis has to be an array of real numbers")
-    if phis.ndim > 1:
-        raise ValueError("phis has to be 1-dimensional")
-    if (np.any(phis <= 0)) or (np.any(phis >= 90)):
-        raise ValueError("phis values have to be between 0° and 90°")
-    if not isinstance(width, numbers.Real) or width <= 0:
-        raise TypeError("width has to be a real number >0")
-    if not isinstance(length, numbers.Real) or length <= 0:
-        raise TypeError("length has to be a real number >0")
+        raise ValueError("frequencies has to be 1-dimensional")
+    if not isinstance(incident_angles, np.ndarray) or \
+            not isinstance(incident_angles.item(0), numbers.Real):
+        raise TypeError("incident_angles has to be an array of real numbers")
+    if incident_angles.ndim > 1:
+        raise ValueError("incident_angles has to be 1-dimensional")
+    if (np.any(incident_angles <= 0)) or (np.any(incident_angles >= 90)):
+        raise ValueError("incident_angles have to be between 0° and 90°")
+    if not isinstance(rectangle_width, numbers.Real) or rectangle_width <= 0:
+        raise TypeError("rectangle_width has to be a real number >0")
+    if not isinstance(gap_width, numbers.Real) or gap_width <= 0:
+        raise TypeError("gap_width has to be a real number >0")
     if not isinstance(height, numbers.Real) or height <= 0:
         raise TypeError("height has to be a real number >0")
-    if not isinstance(c, numbers.Real) or c <= 0:
-        raise TypeError("c has to be a real number >0")
+    if not isinstance(speed_of_sound, numbers.Real) or speed_of_sound <= 0:
+        raise TypeError("speed_of_sound has to be a real number >0")
 
     # Initialization
-    phis = phis*np.pi/180  # from degree to radiant
+    incident_angles = incident_angles*np.pi/180  # from degree to radiant
     # vector of corresponding wavelength in air
-    lambda_air = c/frequencies
+    lambda_air = speed_of_sound/frequencies
     k = 2*np.pi/lambda_air  # vector of wavenumbers
     eps = 1E-3  # stop criterion for infinite sum over u
     n_max = 50  # truncation parameter for numbers of outgoing waves
     n_max2 = 2*n_max+1  # counter from -N to N including zero
+    length = gap_width + rectangle_width  # length of spatial period
 
     x_u = np.zeros((1, n_max), dtype=complex)
     n = np.arange(-n_max, n_max+1)[:, np.newaxis]
     previous_frequency = 0
 
     n_frequency = frequencies.size  # number of computed frequencies
-    n_phi = phis.size  # number of computed incident angles
-    s = np.zeros((n_frequency, n_phi))
+    n_phi = incident_angles.size  # number of computed incident angles
+    scattering_coefficient = np.zeros((n_frequency, n_phi))
 
     # calculation
     for iFrequencies in range(0, n_frequency):
         for iPhi in range(0, n_phi):
-            phi_0 = phis[iPhi]
+            phi_0 = incident_angles[iPhi]
             alpha_n = np.cos(phi_0) + n*lambda_air[iFrequencies]/length
             beta_n = np.conj(-(1j*np.emath.sqrt(alpha_n**2-1)))
 
             # helping variables
             k_frequency = k[iFrequencies]
             k_alpha_n = (k_frequency*alpha_n[:]).T
-            jk_alpha_n_over_width = 1j*k_alpha_n/width
+            jk_alpha_n_over_width = 1j*k_alpha_n/gap_width
 
             y_nr = np.zeros((n_max2, n_max2), dtype=complex)
 
@@ -113,11 +115,11 @@ def rectangular(frequencies, phis, width, length, height, c=343):
                         u_max[0, iR-1] = u_max[0, iR-1] + n_max2-1
 
                         if iR == 1:
-                            U_u_n = np.zeros((u_max.item((iR-1))+1, n_max2),
-                                             dtype=complex)
+                            u_un = np.zeros((u_max.item((iR-1))+1, n_max2),
+                                            dtype=complex)
                         elif u_max[0, iR-1] > u_max_element:
-                            U_u_n = np.zeros((u_max.item((iR-1))+1, n_max2),
-                                             dtype=complex)
+                            u_un = np.zeros((u_max.item((iR-1))+1, n_max2),
+                                            dtype=complex)
 
                         if ((iR == 1) or (u_max[0, iR-1] > u_max_element) or
                            ((iFrequencies+1) > previous_frequency)):
@@ -125,35 +127,36 @@ def rectangular(frequencies, phis, width, length, height, c=343):
                             iU = np.arange(0., round(u_max.item((iR-1)))+1,
                                            dtype=int)[:, np.newaxis]
                             uVals = np.tile(iU, (1, n_max2))
-                            k_x_u = iU*np.pi/width
+                            k_x_u = iU*np.pi/gap_width
                             x_u = np.emath.sqrt(k_frequency**2 - k_x_u**2)
 
-                            CaseA = np.less(abs((k_alpha_n-k_x_u)), 1E-3)
-                            CaseA = CaseA.astype(int)
-                            CaseB = np.less(abs((k_alpha_n+k_x_u)), 1E-3)
-                            CaseB = CaseB.astype(int)
-                            CaseC = np.ones(np.shape(CaseA))-(CaseA+CaseB)
-                            CaseC = CaseC.astype(int)
-                            if np.sum(CaseA) > 0:
-                                U_u_n[np.nonzero(CaseA)] = 0.5 *\
-                                    np.exp(1j*uVals[np.nonzero(CaseA)]*np.pi/2)
-                            if np.sum(CaseA) > 0:
-                                U_u_n[np.nonzero(CaseB)] = 0.5 * \
-                                    np.exp(-1j*uVals[np.nonzero(CaseB)]
+                            # Calculation of U_un according to formula(6)
+
+                            case_a = abs((k_alpha_n-k_x_u)) < 1E-3
+                            case_b = abs((k_alpha_n+k_x_u)) < 1E-3
+                            case_c = np.ones(np.shape(case_a))-(case_a+case_b)
+                            if np.sum(case_a) > 0:
+                                u_un[np.nonzero(case_a)] = 0.5 *\
+                                    np.exp(1j*uVals[np.nonzero(case_a)]
+                                           * np.pi/2)
+                            if np.sum(case_a) > 0:
+                                u_un[np.nonzero(case_b)] = 0.5 * \
+                                    np.exp(-1j*uVals[np.nonzero(case_b)]
                                            * np.pi/2)
                             temp_help = \
                                 (jk_alpha_n_over_width /
                                  (k_x_u**2-k_alpha_n**2))\
-                                * ((np.exp(1j*k_alpha_n*width/2))
+                                * ((np.exp(1j*k_alpha_n*gap_width/2))
                                    - ((-1)**uVals *
-                                   (np.exp(-1j*k_alpha_n*width/2))))
-                            U_u_n[np.nonzero(CaseC)] = \
-                                temp_help[np.nonzero(CaseC)]
+                                   (np.exp(-1j*k_alpha_n*gap_width/2))))
+                            u_un[np.nonzero(case_c)] = \
+                                temp_help[np.nonzero(case_c)]
 
+                        # sum of y_nr in equation(9)
                         temp_U = np.sum([(np.sign(iU)+1)*x_u *
                                         np.tanh(1j*x_u*height) *
-                                        np.conjugate(U_u_n[iU, iN-1]) *
-                                        U_u_n[iU, iR-1]],
+                                        np.conjugate(u_un[iU, iN-1]) *
+                                        u_un[iU, iR-1]],
                                         axis=1)
 
                         err = np.abs((previous_temp_U-temp_U)/previous_temp_U)
@@ -163,17 +166,22 @@ def rectangular(frequencies, phis, width, length, height, c=343):
                         if u_max_element < u_max.item((iR-1)):
                             u_max_element = u_max.item((iR-1))
 
-                    y_nr[iN-1, iR-1] = temp_U*width/(k_frequency*length)
+                    # y_nr according to formula(9)
+                    y_nr[iN-1, iR-1] = \
+                        temp_U*gap_width/(k_frequency*length)
 
+            # solve with least-mean-square for r_n in equation(9) with a*r_n=b
             b = np.sin(phi_0)*(2-(abs(np.sign(n))+1)) + \
                 ((y_nr[:, n_max]).reshape(n_max2, 1))
-            A = np.diagflat(beta_n) - y_nr
+            a = np.diagflat(beta_n) - y_nr
+            r_n = np.linalg.lstsq(a, b, 1E-6)
 
-            R_n = np.linalg.lstsq(A, b, 1E-6)
             valid_ids = 1*(np.abs(alpha_n) <= 1)
-            checksum = np.sum((np.abs(R_n[0][np.nonzero(valid_ids)])**2)
+            checksum = np.sum((np.abs(r_n[0][np.nonzero(valid_ids)])**2)
                               * beta_n[np.nonzero(valid_ids)]/np.sin(phi_0))
             abs_error = np.abs(checksum-1)
             print(f'The absolute error is {abs_error}.')
-            s[iFrequencies, iPhi] = 1-np.abs(R_n[0][n_max])**2
-    return pf.FrequencyData(np.transpose(s), frequencies)
+            # Directional scattering coefficient according to formula(3)
+            scattering_coefficient[iFrequencies, iPhi] = \
+                1-np.abs(r_n[0][n_max])**2
+    return pf.FrequencyData(scattering_coefficient.T, frequencies)
--- tests/test_scattering_analytical.py
+++ tests/test_scattering_analytical.py
@@ -10,48 +10,48 @@ def test_rectangular_under_30_degree():
     c_over_L = 343.901
     x = np.linspace(4, 8, num=5)
     result = ik.scattering.analytical.rectangular(
-        x*c_over_L, np.array([30]), 0.50, 1, 0.49, 343.901)
+        x*c_over_L, np.array([30]), 0.50, 0.50, 0.49, 343.901)
     actual = np.reshape(np.array(
         [[0.82], [0.11], [0.75], [0.67], [0.48]]), (1, 5))
     np.testing.assert_allclose(result.freq, actual, atol=1e-2)
 
 
 def test_rectangular_wrong_input():
-    with pytest.raises(TypeError, match='frequency_vector'):
+    with pytest.raises(TypeError, match='frequencies'):
         ik.scattering.analytical.rectangular(
-            np.array([2+3j]), np.array([30]), 0.5, 1, 0.49, 343.901)
+            np.array([2+3j]), np.array([30]), 0.5, 0.5, 0.49, 343.901)
 
-    with pytest.raises(ValueError, match='frequency_vector'):
+    with pytest.raises(ValueError, match='frequencies'):
         ik.scattering.analytical.rectangular(
             np.array([[500, 1000], [1500, 2000]]),
-            np.array([30]), 0.5, 1, 0.49, 343.901)
+            np.array([30]), 0.5, 0.5, 0.49, 343.901)
 
-    with pytest.raises(TypeError, match='phis'):
+    with pytest.raises(TypeError, match='incident_angles'):
         ik.scattering.analytical.rectangular(
-            np.array([500]), np.array([7+3j]), 0.5, 1, 0.49, 343.901)
+            np.array([500]), np.array([7+3j]), 0.5, 0.5, 0.49, 343.901)
 
-    with pytest.raises(ValueError, match='phis'):
+    with pytest.raises(ValueError, match='incident_angles'):
         ik.scattering.analytical.rectangular(
             np.array([500]), np.array([[7, 25], [9, 35]]),
-            0.5, 1, 0.49, 343.901)
+            0.5, 0.5, 0.49, 343.901)
 
-    with pytest.raises(ValueError, match='phis'):
+    with pytest.raises(ValueError, match='incident_angles'):
         ik.scattering.analytical.rectangular(
             np.array([500]), np.array([30, 60, 90, 120]),
-            0.5, 1, 0.49, 343.901)
+            0.5, 0.5, 0.49, 343.901)
 
-    with pytest.raises(TypeError, match='width'):
+    with pytest.raises(TypeError, match='rectangle_width'):
         ik.scattering.analytical.rectangular(
-            np.array([500]), np.array([30]), 0, 1, 0.49)
+            np.array([500]), np.array([30]), 0, 0.5, 0.49)
 
-    with pytest.raises(TypeError, match='length'):
+    with pytest.raises(TypeError, match='gap_width'):
         ik.scattering.analytical.rectangular(
             np.array([500]), np.array([30]), 0.5, (1+5j), 0.49, 343.901)
 
     with pytest.raises(TypeError, match='height'):
         ik.scattering.analytical.rectangular(
-            np.array([500]), np.array([30]), 0.5, 1, -3, 343.901)
+            np.array([500]), np.array([30]), 0.5, 0.5, -3, 343.901)
 
-    with pytest.raises(TypeError, match='c'):
+    with pytest.raises(TypeError, match='speed_of_sound'):
         ik.scattering.analytical.rectangular(
-            np.array([500]), np.array([30]), 0.5, 1, 0.49, -343.901)
+            np.array([500]), np.array([30]), 0.5, 0.5, 0.49, -343.901)
