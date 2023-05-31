a39fb5a84aa88a118caaa1b7cde9aea3ffdc6544
Anne Heimes <ahe@akustik.rwth-aachen.de>
2023-05-31 13:40:45 +0200

    add test and fix code for scattering analytical

--- imkar/scattering/analytical.py
+++ imkar/scattering/analytical.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pyfar as pf
+import warnings
 
 from scipy import integrate
 from scipy.special import jv, hankel1
 
 
-def sinusoid(frequencies, incident_angle, theta, length=0.177, height=0.051,
-             c=343):
+def sinusoid(
+        frequencies, coordinates, structural_Lambda=0.177,
+        structural_Height=0.051, speed_of_sound=343):
     """
     This function computes the scattering coefficient of a sine-shaped surface
     (with infinite extend) for a given set of frequencies and angles.
@@ -16,21 +18,19 @@ def sinusoid(frequencies, incident_angle, theta, length=0.177, height=0.051,
 
     Parameters
     ----------
-    frequency_vector : array, double
+    frequency_vector : array, float
         Vector of frequencies the scattering coefficient is computed at.
-    incident_angle : array, double
-        Vector of incidences phi the scattering coefficient is computed at.
-        Values between -90°-90° allowed.
-    theta : double
-        Out-of-plane incident angle of incoming waves. Values between -90°-90°
-        allowed.
-    length : double
+    coordinates : pf.Coordinates
+        Coordinate object for the incident directions. The up-vector is
+        in y-direction, the structure towards x-direction. Suggestion is to use
+        the default spherical coordinates.
+    structural_Lambda : float
         Structural length of sinusoidal sample. Without an input, the length of
         177mm is used (test sample).
-    height : double
+    structural_Height : float
         Structural height/depths of the sinusoidal sample (peak-to-peak).
         Without an input, the height of 51mm is used (test sample).
-    c : double
+    speed_of_sound : float
         Speed-of-sound used for the computation.
 
     Returns
@@ -48,147 +48,297 @@ def sinusoid(frequencies, incident_angle, theta, length=0.177, height=0.051,
     # check inputs
 
     # Initialization and constants
-    incident_angle = incident_angle*np.pi/180  # from degree to radiant
-    k_structural = 2*np.pi/length  # structural wavenumber
+    Lambda = structural_Lambda
+    H = structural_Height/2  # half peak-to-peak
+    K = 2*np.pi/Lambda  # structural wavenumber
+    frequencies = np.asarray(frequencies, dtype=float)
     # vector of corresponding wavelength in air
-    lambda_air = c/frequencies
+    lambda_air = speed_of_sound/frequencies
     k = 2*np.pi/lambda_air
-    k_theta = k*np.sin(theta)  # account out-of-plane incidence
+
     eta0 = 0  # normalized admittance
-    height = height/2  # half peak-to-peak
 
     # order of maximum reflection
-    n_max = np.ceil(2*length*frequencies.max()/c)
-    s_coeff = np.zeros((frequencies.size(), incident_angle.size()))
+    n_max = np.ceil(2*Lambda*frequencies.max()/speed_of_sound)
+    scattering_coefficient = np.zeros((coordinates.csize, frequencies.size))
+    n_all = np.arange(-n_max, n_max+1, 1)
 
-    for iFrequencies in range (0,np.size(frequencies)):
-        sPhi = np.zeros(np.size(incident_angle),1)
-        for iPhis in range (0,np.size(incident_angle)):
-            alpha_0 = np.cos(incident_angle[iPhis])
-            gamma_0 = np.sin(incident_angle[iPhis])
+    for i_freq in range(0, np.size(frequencies)):
+        for i_coord in range(coordinates.csize):
+            coord = coordinates[i_coord].get_sph().flatten()
+            azimuth = coord[0]
+            incident_angle = coord[1]
+            k_theta = k*np.sin(azimuth)  # account out-of-plane incidence
 
-            alpha_nn = alpha(np.arange(-n_max, n_max+1, 1), k[iFrequencies],
-                            k_theta[iFrequencies], k_structural)
-            alpha_n = alpha_nn[:,0]
-            n = alpha_nn[:,1]
+            # alpha_0 and gamma_0 after (2)
+            alpha_0 = np.cos(incident_angle)
+            gamma_0 = np.sin(incident_angle)
 
-            if np.size(n) == 0 or (np.size(n) == 1 and n == 0):
+            (alpha_n, n_valid) = _alpha_n(n_all, alpha_0, k[i_freq], K)
+
+            if np.size(n_valid) == 0 or (np.size(n_valid) == 1 and n_valid == 0):
                 continue
-
+            # gamma_n after (2)
             gamma_n = np.sin(np.arccos(alpha_n))
 
-            nmax2 = np.max(np.abs(n))
+            nmax2 = np.max(np.abs(n_valid))
             N = 2*nmax2+1
             n_vec = np.arange(-nmax2, nmax2+1, 1)
-            phi_hat = phi_hat(n_vec, k[iFrequencies]*height*gamma_0)
-            U = np.zeros((N))
+            U = np.zeros((N, N))
 
-            for iM in range(0,np.size(n)):
-                for iN in range(0, np.size(n)):
+            # Calculate U_m_n after (A1)
+            for iM in range(len(n_valid)):
+                for iN in range(len(n_valid)):
                     if (n_vec[iM]-n_vec[iN]) % 2:
-                        A = getA(k[iFrequencies], k_theta[iFrequencies],
-                                 height, k_structural, alpha_0, n[iM], n[iN])
-                        U[n[iM]+nmax2+2,n[iN]+nmax2+2] = \
-                            getV(k[iFrequencies], k_theta[iFrequencies],
-                                 height, k_structural, alpha_0, A, n[iM],
-                                 n[iN]) + getW(k[iFrequencies],
-                                               k_theta[iFrequencies], height,
-                                               k_structural, alpha_0, A, n[iM],
-                                               n[iN])
-            A = np.identity(N) - U
-            b = 2*phi_hat
+                        A = _calculate_limit_for_A1(
+                            k[i_freq], H, alpha_n[iM], alpha_n[iN],
+                            n_valid[iM], n_valid[iN])
+                        V = _get_V_after_A1(
+                            k[i_freq], H, K, alpha_n[iM], A,
+                            n_valid[iM], n_valid[iN])
+                        W = _getW(
+                            k[i_freq], alpha_n[iM], H, K, A,
+                            n_valid[iM], n_valid[iN])
+                        U[iM, iN] = V + W
 
-            Phi = np.linalg.lstsq(A, b)
-            R = np.zeros((N))
-            for iM in range(0, np.size(n)):
-                for iN in range(0, np.size(n)):
-                    R[n[iM]+nmax2+2, n[iN]+nmax2+2] = \
-                        1/(2*gamma_n[iM])*Phi[iN] * \
-                            phi_hat(n_vec[iM]-n_vec[iN], k[iFrequencies] *
-                                    height*gamma_n[iM] *
-                                    (gamma_n[iM]+(n_vec[iM]-n_vec[iN]) *
-                                     k_structural*alpha_n[iM] /
-                                     (k[iFrequencies]*gamma_n[iM])+eta0))
+            # Calculate Phi_m after (6)
+            I_U = np.identity(N) - U
+            phi_hat_m = _phi_hat(n_vec, k[i_freq], H, gamma_0)
+            Phi_m = np.linalg.lstsq(I_U, 2*phi_hat_m)[0]
 
-            R = np.sum(R,2)
-            R = R[n+nmax2+2]
+            # Calculate R_m after
+            R = np.zeros((N, N))
+            for iM in range(np.size(n_valid)):
+                for iN in range(np.size(n_valid)):
+                    phi_hat = _phi_hat(
+                        n_vec[iM]-n_vec[iN], k[i_freq], H, gamma_n[iM])
+                    R[iM, iN] = 1/(2*gamma_n[iM])*Phi_m[iN] * phi_hat * (
+                        gamma_n[iM]+(n_vec[iM]-n_vec[iN]) *
+                        K*alpha_n[iM] / (k[i_freq]*gamma_n[iM]) + eta0
+                        )
 
-            normTest = np.sum(np.abs(R)**2*gamma_n[:])/gamma_0
-            R0 = R[n==0]
-            sPhi[iPhis] = 1-np.min(1,np.abs(R0)**2)
-        s_coeff[iPhis,:] = sPhi
-    s_coeff = np.column_stack(s_coeff, sPhi)
+            R_m = np.sum(R, 1)
+            R_m = R_m[n_valid+nmax2+1]
 
-    # helping functions
-    # -------------------------------------------------------------------------
-    def alpha(n, alpha0, k, k_theta, k_structural):
-        alpha_f = (k_theta/k)**2 + alpha0 + n*k_structural/k
-        valid_alpha = (np.abs(alpha_f) <= 1)
-        n = n[np.nonzero(valid_alpha)]
-        alpha_f = alpha_f[np.nonzero(valid_alpha)]
+            normTest = np.sum(np.abs(R_m)**2*gamma_n)/gamma_0
+            R_0 = R_m[n_valid == 0][0]
+            scattering_coefficient[i_coord, i_freq] = 1 - np.min(
+                (1, np.abs(R_0)**2))
+    return pf.FrequencyData(scattering_coefficient, frequencies)
 
-        return np.column_stack((alpha_f, n))
 
-    def phi_hat(m, k_h_gamma0):
-        phi_f = np.zeros((np.size(m), 1))
-        for iM in range(0, np.size(m)):
-            phi_f[iM] = (-1)**m[iM]*jv(m[iM], k_h_gamma0)
+def _alpha_n(n, alpha_0, k, K):
+    """Calculate alpha_n after Formula 2
 
-        return phi_f
+    Parameters
+    ----------
+    n : int
+        index n
+    alpha_0 : float
+        cos(azimuth) of incident angle azimuth
+    k : float
+        Wave number of the sound in air
+    K : float
+        Structural wave number of the sample 2pi/Lambda
 
-    def getA(k, k_theta, height, k_structural, alpha0, m, n):
-        if n == m+1 or n == m-1:
-            alpha_m = alpha(m, alpha0, k, k_theta, k_structural)
-            alpha_n = alpha(n, alpha0, k, k_theta, k_structural)
-            if np.abs(alpha_m) == 1 or np.abs(alpha_n) == 1:
-                A = 1/k*np.max(40, 200*(k*height)**2)
-            else:
-                A = 1/k*np.max(40, 200*(k*height)**2, 50/np.min
-                               (1+alpha_m, 1-alpha_m, 1-alpha_n, 1+alpha_n))
+    Returns
+    -------
+    alpha_n : float
+        returns alpha_n
+    """
+    # K/k = lambda/Lambda
+    n = np.asarray(n)
+    alpha_n = alpha_0 + n*K/k
+    valid_alpha = np.abs(alpha_n) <= 1
+    n = np.array(n[valid_alpha], dtype=int)
+    alpha_n = alpha_n[valid_alpha]
+
+    return (alpha_n, n)
+
+
+def _phi_hat(m, k, H, gamma_0):
+    """
+    Calculates p_hat after A8 with bessel function
+
+    Parameters
+    ----------
+    m : array, int
+        Index m for p_hat
+    k : float
+        Wave number of the sound in air
+    H : float
+        Hight of the sample surface structure in meter
+    gamma_0 : float
+        Describes the sin(Phi_0), where Phi_0 in the
+        incident sound direction
+
+    Returns
+    -------
+    p_hat: array, number
+        result of shape of m
+    """
+    m = np.asarray(m)
+    phi_f = (-1j)**m * jv(m, k*H*gamma_0)
+
+    return phi_f
+
+
+def _calculate_limit_for_A1(k, H, alpha_m, alpha_n, m, n):
+    """Calculates the limit A for the integral in (A7)
+
+    Parameters
+    ----------
+    k : number
+        Wave number of the sound in air
+    H : number
+        Hight of the sample surface structure in meter
+    alpha_m : number
+        Describes the cos(Phi_0) + m*lambda/Lambda, where Phi_0 in the
+        incident sound direction
+    alpha_n : number
+        Describes the cos(Phi_0) + n*lambda/Lambda, where Phi_0 in the
+        incident sound direction
+    m : int
+        index m
+    n : int
+        index n
+
+    Returns
+    -------
+    A : number
+        limit for integration
+    """
+    if n == m+1 or n == m-1:
+        if np.abs(alpha_m) == 1 or np.abs(alpha_n) == 1:
+            A = np.max((40, 200*(k*H)**2))/k
         else:
-            A = 1/k*np.max(40, 200*(k*height)**2)
+            A = np.max((40, 200*(k*H)**2, 50/np.min(np.abs((
+                1+alpha_m, 1-alpha_m, 1-alpha_n, 1+alpha_n)))))/k
+    else:
+        A = np.max((40, 200*(k*H)**2))/k
 
-        return A
+    return A
 
-    def int2func(s, t, h, k, k_structural):
-        # for small values of t the hankel function explodes due to t~rho
-        if np.abs(t) < 0.001:
-            res = -(h*k_structural**2)/(np.pi*2) *\
-                 (np.cos(k_structural*s)-k_structural*t/3 *
-                  np.sin(k_structural*s)) /\
-                 (1+(h*k_structural*np.sin(k_structural*s))**2+t*h**2 *
-                  k_structural**3*np.sin(k_structural*s) *
-                  np.cos(k_structural*s))
-        else:
-            rho = np.sqrt(t**2+(h*np.cos(k_structural*(s+t)) -
-                                h*np.cos(k_structural*s))**2)
-            B = h*np.cos(k_structural*(s+t))-h*np.cos(k_structural*s)-t*(-h) *\
-                k_structural*np.sin(k_structural*s)
-            res = 1j*k/(2*rho)*hankel1(1, k*rho)*B
 
-        return res
+def _get_V_integral_2(x, tau, H, k, K):
+    """Second part of formula A1
 
-    def getV(k, k_theta, height, k_structural, alpha0, A, m, n):
-        alpha_m = alpha(m, alpha0, k, k_theta, k_structural)
-        # add code here
+    Parameters
+    ----------
+    x : number
+        integral parameter x
+    tau : number
+        integral parameter tau
+    H : number
+        Hight of the sample surface structure in meter
+    k : number
+        Wave number of the sound in air
+    K : number
+        Structural wave number of the sample 2pi/Lambda
 
-    def getW(k, k_theta, height, k_structural, alpha0, A, m, n):
-        if n == m+1 or n == m-1:
-            alpha_m = alpha(m, alpha0, k, k_theta, k_structural)
-            if abs(alpha_m) == 1:
-                W = 0
-            else:
-                W = 1j*height*k_structural/(1-alpha_m**2) /\
-                    np.sqrt(2*np.pi*k*A)*np.exp(1j*(k*A-0.75*np.pi)) *\
-                    (alpha_m*np.cos(k*A*alpha_m)-1j*np.sin(k*A*alpha_m))
-                if n == m+1:
-                    W = -W
-        else:
+    Returns
+    -------
+    result: number
+        result for the second
+    """
+    x = np.asarray(x, dtype=float)
+    # for small values of t the Hankel function explodes due to t~rho
+    if np.abs(tau) < 0.001:
+        # Formula A9 without the exponential term, this is added in the later
+        # after that
+        return -(H*K**2)/(np.pi*2) * (
+            np.cos(K*x)-K*tau/3 * np.sin(K*x)) / (
+            1 + (H*K*np.sin(K*x))**2 + tau*H**2*K**3*np.sin(K*x)*np.cos(K*x)
+            )
+    else:
+        rho = np.sqrt(tau**2 + (H*np.cos(K*(x+tau)) - H*np.cos(K*x))**2)
+        xi = lambda x: H*np.cos(K*(x))
+        dxi = lambda x: -K*H*np.sin(K*(x))
+        B = xi(x+tau) - xi(x) - tau*dxi(x)
+        return 1j*k/(2*rho) * hankel1(1, k*rho) * B
+
+
+def _get_V_after_A1(k, H, K, alpha_m, A, m, n):
+    """Solves formula A1 with the following first assumptions described
+    in the paragraph after it. All parameter names are same as in the paper.
+
+    Parameters
+    ----------
+    k : number
+        Wave number of the sound in air
+    H : number
+        Hight of the sample surface structure in meter
+    K : number
+        Structural wave number of the sample 2pi/Lambda
+    alpha_m : number
+        Describes the cos(Phi_0) + m*lambda/Lambda, where Phi_0 in the
+        incident sound direction
+    A : number
+        Limits of the second integral
+    m : int
+        index m
+    n : int
+        index n
+
+    Returns
+    -------
+    V : number
+        _description_
+    """
+    pi = np.pi
+    # implements A1 here with the limits from -A and A, and the 0 to Lambda/2
+    # Note that K/pi = 2/Lambda
+    A1 = lambda x, tau: K/pi*np.exp(-1j*(m-n)*K*x) * _get_V_integral_2(
+        x, tau, H, k, K) * np.exp(-1j*k*alpha_m*tau)
+    # Note that pi/K = Lambda/2
+    A1_tau = lambda tau: integrate.quad(
+        A1, 0, pi/K, args=(tau), complex_func=True, limit=3000)[0]
+    V = integrate.quad(A1_tau, -A, A, complex_func=True, limit=3000)
+    if np.abs(V[1]) > 2e-8:
+        warnings.warn(f'Error is higher than 2e-8, its {np.abs(V[1])}')
+    print(f'Error is higher than 2e-8, its {np.abs(V[1])}')
+    return V[0]
+
+
+def _getW(k, alpha_m, H, K, A, m, n):
+    """Calculate W after Formula (A6)
+
+    Parameters
+    ----------
+    k : _type_
+        _description_
+    alpha_m : _type_
+        _description_
+    H : _type_
+        _description_
+    K : _type_
+        _description_
+    A : _type_
+        _description_
+    m : _type_
+        _description_
+    n : _type_
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    pi = np.pi
+    if n == m+1 or n == m-1:
+        if np.abs(alpha_m) == 1:
             W = 0
+        else:
+            W = 1j*H*K/(1-alpha_m**2) / np.sqrt(2*pi*k*A) * np.exp(
+                1j*(k*A-0.75*pi)) * (
+                alpha_m*np.cos(k*A*alpha_m)-1j*np.sin(k*A*alpha_m))
+            if n == m+1:
+                W = -W
+    else:
+        W = 0
 
-        return W
-
-    return pf.FrequencyData(np.transpose(s_coeff), frequencies)
+    return W
 
 
 def holford_urusovskii_method(ms, ns, Lambda, h, xi, dxi, k, Phi_0):
--- tests/test_scattering_analytical.py
+++ tests/test_scattering_analytical.py
@@ -1,19 +1,206 @@
 import imkar as ik
 import numpy as np
+import pyfar as pf
+from scipy.special import hankel1
+import numpy.testing as npt
+import pytest
 
 
-def test_holford_urusovskii_method():
-    h = 0.051/2
+def test_same_result_for_increasing_A():
+    H = 0.0255
+    k = 20.270143083466690
+    K = 35.498222074460940
+    alpha_0 = 0.766044443118978
+    m = -1
+    n = 0
+    (alpha_m, m) = ik.scattering.analytical._alpha_n(m, alpha_0, k, K)
+    (alpha_n, n) = ik.scattering.analytical._alpha_n(n, alpha_0, k, K)
+
+    A0 = ik.scattering.analytical._calculate_limit_for_A1(
+        k, H, alpha_n, alpha_m, n, m)
+    for factor in [1, 2, 4, 8, 16]:
+        A = A0*factor
+
+        V = ik.scattering.analytical._get_V_after_A1(
+            k, H, K, alpha_n, A, m, n)
+        W = ik.scattering.analytical._getW(
+            k, alpha_n, H, K, A, m, n)
+
+        print(f'{factor} => V+W = {V+W}, V = {V}, W = {W}')
+
+    assert True
+
+
+def test_sine():
+    f = [
+        1.029559118236473e+03, 1.097194388777555e+03, 1.886272545090180e+03,
+        2.171843687374749e+03, 3.021042084168336e+03]
+    coordinates = pf.Coordinates(0, 40, 1, 'sph', 'top_colat', 'deg')
+    desired = [
+        0, 0.954209748892172, 0.908419497784343, 0, 0.968980797636632]
+    s = ik.scattering.analytical.sinusoid(
+        f, coordinates, speed_of_sound=340.1)
+    npt.assert_allclose(s.freq.flatten(), np.array(desired))
+
+
+def test__getW():
+    Phi_0 = 40
+    c = 343
+    f = 1000
     Lambda = 0.177
-    f = 2176
-    k = 2*np.pi / (343/f)
-    Phi_0 = 40 * np.pi/180
-    K = 2 * np.pi / Lambda
-    xi = lambda x: h*np.cos(K*x)
-    dxi = lambda x: -K*h*np.sin(K*x)
-    ms = np.arange(-3, 3)
-    ns = np.arange(-3, 3)
-    U_m_n = ik.scattering.analytical.holford_urusovskii_method(
-        ms, ns, Lambda, h, xi, dxi, k, Phi_0)
-    assert 1
+    k = 2*np.pi / (c/f)
+    H = 0.0255
+    K = 2*np.pi / 0.177
+    ldL = (c/f) / Lambda
+    m = 0
+    n = 1
+    alpha_m = np.cos(Phi_0) + m*ldL
+    alpha_n = np.cos(Phi_0) + n*ldL
+    A = ik.scattering.analytical._calculate_limit_for_A1(
+        k, H, alpha_m, alpha_n, m, n)
 
+    W = ik.scattering.analytical._getW(k, alpha_m, H, K, A, m, n)
+    assert W == -0.02418445918393081-0.02121370543325804j
+
+
+def test__alpha_n():
+    incident_angle = 45*np.pi/180
+    k = 2*np.pi / (343/4000)
+    K = 2*np.pi / 0.177
+    n_all = np.arange(-5, 6)
+    alpha_0 = np.cos(incident_angle)
+    (alpha_n, n_valid) = ik.scattering.analytical._alpha_n(
+        n_all, alpha_0, k, K)
+
+    npt.assert_allclose(alpha_n.shape, n_valid.shape)
+    npt.assert_allclose(alpha_n, np.array(
+            [-0.74628305, -0.26181977, 0.2226435, 0.70710678]))
+    npt.assert_array_equal(n_valid, np.array([-3, -2, -1, 0]))
+
+
+@pytest.mark.parametrize("Phi_0",  [0, 30, 60])
+@pytest.mark.parametrize("m",  [-1, 0, 1])
+@pytest.mark.parametrize("n",  [-1, 0, 1])
+def test__calculate_limit_for_A1(Phi_0, m, n):
+    # test the limits discussed in the first paragraph of the Appendix
+    c = 343
+    f = 1000
+    Lambda = 0.177
+    k = 2*np.pi / (c/f)
+    H = 0.0255
+    ldL = (c/f) / Lambda
+    alpha_m = np.cos(Phi_0) + m*ldL
+    alpha_n = np.cos(Phi_0) + n*ldL
+    A = ik.scattering.analytical._calculate_limit_for_A1(
+        k, H, alpha_m, alpha_n, m, n)
+
+    # 17.3H < A
+    npt.assert_array_less(17.3*H, A)
+    # 200 k H^2 < A
+    npt.assert_array_less(200*k*H**2, A)
+    # 40 < kA
+    npt.assert_array_less(40, k*A)
+
+
+def test__phi_hat():
+    # compare with matlab results (itaToolbox)
+    m = [-1, 0, 1]
+    k = 39.494515074304640
+    H = 0.0255
+    gamma_0 = 0.642787609686539
+    p_hat = ik.scattering.analytical._phi_hat(m, k, H, gamma_0)
+    desired = np.array([
+        -0.000000000000000 - 0.307016844838359j,
+        0.897944283825281 + 0.000000000000000j,
+        0.000000000000000 - 0.307016844838359j])
+    npt.assert_almost_equal(p_hat, desired)
+
+
+def test__get_V_integral_2_for_ml2():
+    # compare with matlab
+    tau = -7.509435633700259e-04
+    x = [4.844703797163286e-08, 0.022517796477943, 0.067032311870945]
+    H = 0.0255
+    k = 1.441658700529420e+02
+    K = 35.49822207446094
+
+    desired = np.asarray([
+        -5.114150908090244,
+        -2.551442223000378,
+        2.617393390501456])
+    actual = ik.scattering.analytical._get_V_integral_2(x, tau, H, k, K)
+
+    assert not any(np.isnan(desired))
+    npt.assert_allclose(actual, desired)
+
+
+def test__get_V_integral_2_for_ml():
+    # compare with matlab
+    tau = -18.748750873324310
+    x = [4.844703797163286e-08, 0.022517796477943, 0.067032311870945]
+    H = 0.025500000000000
+    k = 1.441658700529420e+02
+    K = 35.498222074460940
+
+    actual = np.asarray([
+        -1.536121864092959e-04 - 6.047826161877591e-05j,
+        -0.668447984135646 - 0.263459949629663j,
+        -0.643611876916951 - 0.253474931840364j])
+    result = ik.scattering.analytical._get_V_integral_2(x, tau, H, k, K)
+
+    assert not any(np.isnan(result))
+    npt.assert_allclose(actual, result)
+
+
+def test__get_V_integral_2_for_large_tau():
+    # todo compare with matlab results (itaToolbox)
+    tau = 5
+    x = 1
+    H = 0.05
+    k = 1000/343*(2*np.pi)
+    K = 2*np.pi / 0.177
+
+    rho = np.sqrt(tau**2 + (H*np.cos(K*(x+tau)) - H*np.cos(K*x))**2)
+    xi = lambda x: H*np.cos(K*(x))
+    dxi = lambda x: -K*H*np.sin(K*(x))
+    B = xi(x+tau) - xi(x) - tau*dxi(x)
+    actual = 1j*k/(2*rho) * hankel1(1, k*rho) * B
+    result = ik.scattering.analytical._get_V_integral_2(x, tau, H, k, K)
+
+    assert not np.isnan(result)
+    assert actual == result
+
+
+def test__get_V_integral_2_for_small_tau():
+    # todo compare with matlab results (itaToolbox)
+    tau = 5e-5
+    x = 1
+    H = 0.05
+    k = 1000/343*(2*np.pi)
+    K = 2*np.pi / 0.177
+
+    actual = -(H*K**2)/(np.pi*2) * (
+        np.cos(K*x)-K*tau/3 * np.sin(K*x)) / (
+        1 + (H*K*np.sin(K*x))**2 + tau*H**2*K**3*np.sin(K*x)*np.cos(K*x)
+        )
+    result = ik.scattering.analytical._get_V_integral_2(x, tau, H, k, K)
+
+    assert not np.isnan(result)
+    assert actual == result
+
+
+def test__get_V_after_A1():
+    # todo compare with matlab results (itaToolbox)
+    k = 1.441658700529420e+02
+    H = 0.025500000000000
+    K = 35.49822207446094
+    alpha_m = -0.942186278912206
+    A = 18.7487714003851
+    m = -5
+    n = -4
+
+    result = ik.scattering.analytical._get_V_after_A1(
+        k, H, K, alpha_m, A, m, n)
+
+    actual = 0.536526156484156 - 0.635367204656775j
+    npt.assert_almost_equal(actual, result, decimal=4)
