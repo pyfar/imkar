35e3ac5566cb24c831f436aacdf38be5f72203bb
Maxnilo1234 <Maxnilo1234@gmail.com>
2023-07-05 14:48:51 +0200

    First implementation of scattering_diffuse

--- imkar/scattering/diffuse.py
+++ imkar/scattering/diffuse.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def diffuse(reverb_time, speed_of_sound, air_attenuation,
              alpha_method = 'Eyring',scale_factor = 5, sample_area = 0.5027,
-                surface_room = 9.05, volume_room = 1.67):
+                baseplate_area = 0.58088, surface_room = 9.05, 
+                    volume_room = 1.67):
     """
     This function calculates the diffuse scattering coefficient after the ISO
     17497-1 method. It uses four reverberation times measured under different
@@ -37,6 +38,10 @@ def diffuse(reverb_time, speed_of_sound, air_attenuation,
         Surface area of the measurement sample in square meter. Standard value
         according to the test sample at IHTA.
 
+    baseplate_area : float
+        Surface area of the baseplate in square meter. Standard value according
+        to the measurement chamber at IHTA.
+
     surface_room : float
         Surface area of the measurement room in square meter. Standard value
         according to measurement chamber at IHTA.
@@ -61,5 +66,23 @@ def diffuse(reverb_time, speed_of_sound, air_attenuation,
     # Scale frequencies
     reverb_time[:,1] = reverb_time[:,1]/scale_factor
 
+    # random-incidence absorption coefficient
+    a_s = 55.3*volume_room/sample_area*(1/(speed_of_sound[1]*reverb_time[1])
+            -1/(speed_of_sound[0]*reverb_time[0]))-4*volume_room/sample_area* \
+                (air_attenuation[1]-air_attenuation[0])
+    
+    # specular absorption coefficient
+    a_spec = 55.3*volume_room/sample_area*(1/(speed_of_sound[3]*reverb_time[3])
+            -1/(speed_of_sound[2]*reverb_time[2]))-4*volume_room/sample_area* \
+                (air_attenuation[3]-air_attenuation[2])
+    
+    # scattering coefficient for the base plate
+    s_base = 55.3*volume_room/sample_area*(1/(speed_of_sound[2]*reverb_time[2])
+            -1/(speed_of_sound[0]*reverb_time[0]))-4*volume_room/sample_area* \
+                (air_attenuation[2]-air_attenuation[0])
+    
+    # random-incidence scattering coefficient
+    scatterring_coefficient = 1-(1-a_spec)/(1-a_s)
 
-    pass
+    return scatterring_coefficient
+    
