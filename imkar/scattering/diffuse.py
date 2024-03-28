0af30bc5e2d1697ed5b2a69e2c8dcb08bee8c6aa
Anne Heimes <ahe@akustik.rwth-aachen.de>
2024-03-28 12:40:38 +0100

    started clean up

--- imkar/scattering/diffuse.py
+++ imkar/scattering/diffuse.py
@@ -1,6 +1,5 @@
 import pyfar as pf
 import numpy as np
-import numbers
 
 
 def diffuse(reverberation_time, speed_of_sound, air_attenuation,
@@ -19,38 +18,29 @@ def diffuse(reverberation_time, speed_of_sound, air_attenuation,
         the four different measurements. If the measurement is performed at
         multiple positions, the mean value for every condition of the
         measurements is taken. Its cshape has to be (..., 4).
-
     speed_of_sound : array, float
         Array containing the speed of sound in m/s for every measurement
-        condition calculated by the temperature and air humidity during
-        measurement time.
-
-    air_attenuation : array, pf.FrequencyData
+        condition during the measurement.
+    air_attenuation : pf.FrequencyData
         Matrix or FrequencyData containing the air attenuation in 1/m for every
         measurement condition calculated by temperature and air humidity during
         measurement time. Its cshape has to be (..., 4).
-
     calculation_method : 'ISO', 'Eyring' or 'Sabine'
         Defines the method by which alpha is calculated.
         For method 'ISO' the baseplate_area and sample_area need to be equal.
-
     scale_factor : float
         Factor that corrects the frequencies when using a smaller scale
-        reverbaration room than defined in the ISO standard. Standard value is
+        reverberation room than defined in the ISO standard. Standard value is
         5, according to the measurement chamber at IHTA.
-
     sample_area : float
         Surface area of the measurement sample in square meter. Standard value
         according to the test sample.
-
     baseplate_area : float
         Surface area of the baseplate in square meter. Standard value according
         to the measurement chamber at IHTA.
-
     surface_room : float
         Surface area of the measurement room in square meter. Standard value
         according to measurement chamber at IHTA.
-
     volume_room : float
         Volume of the empty measurement room in cubic meter. Standard value
         according to measurement chamber at IHTA.
@@ -73,10 +63,7 @@ def diffuse(reverberation_time, speed_of_sound, air_attenuation,
     if reverberation_time.cshape[-1] != 4:
         raise ValueError("reverberation_time.cshape has to be (..., 4)")
 
-    if (np.asarray(speed_of_sound, dtype=float).ndim == 1):
-        speed_of_sound = (np.asarray(speed_of_sound, dtype=float)[:, None]).T
-    else:
-        speed_of_sound = (np.asarray(speed_of_sound, dtype=float))
+    speed_of_sound = np.asarray(speed_of_sound, dtype=float)
 
     if (speed_of_sound.shape != reverberation_time.cshape):
         raise ValueError("speed_of_sound has to be an array containing the \
@@ -87,17 +74,14 @@ def diffuse(reverberation_time, speed_of_sound, air_attenuation,
     if (np.any(np.asarray(speed_of_sound, dtype=float) <= 0)):
         raise ValueError("speed_of_sound has to be a value bigger than zero")
 
-    if isinstance(air_attenuation, pf.FrequencyData):
-        air_attenuation = np.asarray(air_attenuation.freq, dtype=float)
-    else:
-        air_attenuation = np.asarray(air_attenuation, dtype=float)
-    if air_attenuation.shape != (reverberation_time.cshape +
-                                 (reverberation_time.n_bins, )):
-        raise ValueError("air_attenuation has to be of shape (..., 4,\
-                         number_of_frequencies) and equal the shape of \
-                         reverberation_time.")
+    if not isinstance(air_attenuation, pf.FrequencyData):
+        raise ValueError("air_attenuation has to be of type pf.FrequencyData.")
 
-    if (np.any(air_attenuation < 0)):
+    if air_attenuation.freq.shape != reverberation_time.freq.shape:
+        raise ValueError("air_attenuation and reverberation_time has to have \
+                         cshape and number number of frequency bins.")
+
+    if (np.any(air_attenuation.freq < 0)):
         raise ValueError("air_attenuation has to be a value bigger or equal \
                          to zero")
 
@@ -109,40 +93,40 @@ def diffuse(reverberation_time, speed_of_sound, air_attenuation,
         raise ValueError("For calculation method 'ISO' the baseplate_area and \
                          sample_area need to be equal.")
 
-    if not isinstance(scale_factor, numbers.Real) or scale_factor <= 0:
+    if not isinstance(scale_factor, (int, float)) or scale_factor <= 0:
         raise TypeError("scale_factor has to be a real number >0")
 
-    if not isinstance(sample_area, numbers.Real) or sample_area <= 0:
+    if not isinstance(sample_area, (int, float)) or sample_area <= 0:
         raise TypeError("sample_area has to be a real number >0")
 
-    if not isinstance(baseplate_area, numbers.Real) or baseplate_area <= 0:
+    if not isinstance(baseplate_area, (int, float)) or baseplate_area <= 0:
         raise TypeError("baseplate_area has to be a real number >0")
 
-    if not isinstance(surface_room, numbers.Real) or surface_room <= 0:
+    if not isinstance(surface_room, (int, float)) or surface_room <= 0:
         raise TypeError("surface_room has to be a real number >0")
 
-    if not isinstance(volume_room, numbers.Real) or volume_room <= 0:
+    if not isinstance(volume_room, (int, float)) or volume_room <= 0:
         raise TypeError("volume_room has to be a real number >0")
 
     # start calculations
 
     if (calculation_method == "ISO"):
         # random-incidence absorption coefficient
-        a_s = (55.3*volume_room/sample_area *
+        alpha_s = (55.3*volume_room/sample_area *
                (1/(speed_of_sound[:, 1]*reverberation_time.freq[:, 1])-1 /
                 (speed_of_sound[:, 0]*reverberation_time.freq[:, 0])) -
                4*volume_room/sample_area *
-               (air_attenuation[:, 1]-air_attenuation[:, 0]))
+               (air_attenuation[..., 1, :]-air_attenuation[..., 0, :]))
 
         # specular absorption coefficient
-        a_spec = (55.3*volume_room/sample_area *
+        alpha_spec = (55.3*volume_room/sample_area *
                   (1/(speed_of_sound[:, 3]*reverberation_time.freq[:, 3])
                    - 1/(speed_of_sound[:, 2]*reverberation_time.freq[:, 2])) -
                   4*volume_room/sample_area *
                   (air_attenuation[:, 3]-air_attenuation[:, 2]))
 
         # scattering coefficient for the base plate
-        s_base = (55.3*volume_room/sample_area *
+        scattering_base = (55.3*volume_room/sample_area *
                   (1/(speed_of_sound[:, 2]*reverberation_time.freq[:, 2])
                    - 1/(speed_of_sound[:, 0]*reverberation_time.freq[:, 0])) -
                   4*volume_room/sample_area *
@@ -150,32 +134,33 @@ def diffuse(reverberation_time, speed_of_sound, air_attenuation,
 
     else:
         # calculate equivalent surface area
-        A = np.zeros((reverberation_time.cshape +
-                      (reverberation_time.n_bins, )))
-        for i in range(0, 4):
-            A[:, i] = volume_room * \
-                ((24*np.log(10)) /
-                 (speed_of_sound[:, i][:, None]*reverberation_time.freq[:, i])
-                    - 4*air_attenuation[:, i])
+        A = volume_room * (
+            (24*np.log(10)) / (speed_of_sound*reverberation_time)
+            - 4*air_attenuation)
 
         # calculate alpha based on chosen method
         if (calculation_method == "Eyring"):
-            alpha = 1-np.exp(-A/surface_room)
+            alpha = pf.FrequencyData(
+                1-np.log(A.freq/(-surface_room)), A.frequencies)
         elif (calculation_method == "Sabine"):
             alpha = A/surface_room
 
+        alpha_1 = alpha.freq[..., 0, :]
+        alpha_2 = alpha.freq[..., 1, :]
+        alpha_3 = alpha.freq[..., 2, :]
+        alpha_4 = alpha.freq[..., 3, :]
         # random-incidence absorption coefficient
-        a_s = surface_room/sample_area*(alpha[:, 1]-alpha[:, 0])+alpha[:, 0]
+        alpha_s = surface_room/sample_area*(alpha_2-alpha_1)+alpha_1
 
         # specular absorption coefficient
-        a_spec = surface_room/sample_area*(alpha[:, 3]-alpha[:, 2])+alpha[:, 2]
+        alpha_spec = surface_room/sample_area*(alpha_4-alpha_3)+alpha_3
 
         # scattering coefficient for the base plate
-        s_base = surface_room/baseplate_area * \
-            (alpha[:, 2]-alpha[:, 0]).clip(min=0)
+        scattering_base = surface_room/baseplate_area * \
+            (alpha_3-alpha_1).clip(min=0)
 
     # random-incidence scattering coefficient
-    scatterring_coefficient = (1-(1-a_spec)/(1-a_s)).clip(min=0)
+    scatterring_coefficient = (1-(1-alpha_spec)/(1-alpha_s)).clip(min=0)
 
-    return pf.FrequencyData([scatterring_coefficient, s_base],
+    return pf.FrequencyData([scatterring_coefficient, scattering_base],
                             reverberation_time.frequencies/scale_factor)
--- tests/test_scattering_diffuse.py
+++ tests/test_scattering_diffuse.py
@@ -11,21 +11,17 @@ def test_diffuse_eyring():
     T60_2 = [0.7, 0.58, 0.55, 0.46, 0.38, 0.23, 0.15]   # T2
     T60_3 = [0.91, 0.71, 0.73, 0.59, 0.48, 0.27, 0.17]  # T3
     T60_4 = [0.71, 0.58, 0.50, 0.30, 0.26, 0.18, 0.13]  # T4
-    reverb_time = pf.FrequencyData([[T60_1, T60_2, T60_3, T60_4]], f)
+    reverb_time = pf.FrequencyData([T60_1, T60_2, T60_3, T60_4], f)
     speed_of_sound = [344.5287, 344.5287, 344.5287, 344.5287]
-    air_attenuation_s = [0.000652350693755930, 0.00111221984304401,
-                         0.00226837553058824, 0.00659994837403191,
-                         0.0232464500602580, 0.0815039446237653,
-                         0.167026524991143]
-    air_attenuation = [[air_attenuation_s, air_attenuation_s,
-                       air_attenuation_s, air_attenuation_s]]
+    air_attenuation = pf.FrequencyData(np.tile([
+        0.000652350693755930, 0.00111221984304401, 0.00226837553058824,
+        0.00659994837403191, 0.0232464500602580, 0.0815039446237653,
+        0.167026524991143], (4,1)), f)
 
-    air_attenuation = pf.FrequencyData(air_attenuation, f)
+    scattering_coefficient = ik.scattering.diffuse(
+        reverb_time, speed_of_sound, air_attenuation, 'Eyring')
 
     desired_eyring = [[0, 0, 0.1359, 0.8021, 0.8808, 0.9331, 0.8963]]
-
-    scattering_coefficient = ik.scattering.diffuse(reverb_time, speed_of_sound,
-                                                   air_attenuation, 'Eyring')
 
     npt.assert_almost_equal(scattering_coefficient.freq[0],
                             desired_eyring, 3)
